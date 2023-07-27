878c16d8ec0b38d304e97ce94a9f4647d36cc69d
Pyankov Daniil <pyankovdaniil5@gmail.com>
2023-07-27 14:35:55 +0300

    Fixed .sql data

--- src/main/resources/db/v1_insert_cards_service_data.sql
+++ src/main/resources/db/v1_insert_cards_service_data.sql
@@ -1,18 +1,18 @@
 INSERT INTO bank_account (account_number, first_name, last_name, patronymic)
 VALUES
-    ('63ebd05b-a67e-47d3-99f4-b8f7306b52cd', 'Alexey', 'Ivanov', 'Dmitrievich'), --Default Card
+    ('63ebd05b-a67e-47d3-99f4-b8f7306b52cd', 'Alexey', 'Ivanov', 'Dmitrievich'), -- DEFAULT_CARD
     ('f2dea26d-4d41-45ac-8bb6-90b7d58447ec', 'Petr', 'Petrov', 'Danilovich'), -- HIPSTER_DEFAULT
     ('5cc4ed6a-c2c7-4163-bc29-bc76c3ee9cad', 'Alexandr', 'Smirnov', 'Petrovich'), -- HIPSTER_GOLD
-    ('ea849672-5368-4014-ab74-b9639cfe747f', 'Dmitrii', 'Sobolev', 'Evgenievich'), --  HIPSTER_PlATINUM
-    ('105e50b1-4cd7-491f-8d0d-0c308968bec0', 'Semen', 'Vorobiev', 'Alexandrovich'); -- HIPSTER BLACK
+    ('ea849672-5368-4014-ab74-b9639cfe747f', 'Dmitrii', 'Sobolev', 'Evgenievich'), -- HIPSTER_PLATINUM
+    ('105e50b1-4cd7-491f-8d0d-0c308968bec0', 'Semen', 'Vorobiev', 'Alexandrovich'); -- HIPSTER_BLACK
 
 INSERT INTO card_type (id, type_name, cashback_percent, min_balance_for_cashback)
 VALUES
-    (991, 'DEFAULT_CARD', 0.5, 30000),
-    (992, 'HIPSTER_DEFAULT', 1, 50000),
-    (993, 'HIPSTER_GOLD', 1.5, 70000),
-    (994, 'HIPSTER_PLATINUM', 2, 90000),
-    (995, 'HIPSTER_BLACK', 2.5, 110000);
+    (991, 'DEFAULT_CARD', 0, 0),
+    (992, 'HIPSTER_DEFAULT', 1, 30000),
+    (993, 'HIPS TER_GOLD', 1.5, 50000),
+    (994, 'HIPSTER_PLATINUM', 2, 70000),
+    (995, 'HIPSTER_BLACK', 2.5, 90000);
 
 INSERT INTO card (id, account_number, card_type_id)
 VALUES
